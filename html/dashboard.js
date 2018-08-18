71499f4f3017f3f3bbaaa427e8e976e0606f3405
plancker <kshitij.kapoor@ashoka.edu.in>
2018-08-18 08:33:17 +0530

    Somemore changes on task dashboard

--- html/dashboard.js
+++ html/dashboard.js
@@ -3,7 +3,27 @@ chrome.storage.local.get("TASKS", function(tasksObject){
     chrome.storage.local.get("CTASKID", function(cTaskIdObject){
       if(cTaskIdObject["CTASKID"]){
         const task = tasksObject["TASKS"][cTaskIdObject["CTASKID"]];
-        $("#taskNameBanner").text(task.name);
+
+        $("#taskNameBanner").text(task.name); //Add task Name
+
+        $("#lastOpenedTime").text("LAST OPENED: " + task.activationTime[task.activationTime.length-1].split("GMT")[0]);
+
+        var timeSpent = 0;
+
+        for(var i =0; i<task.deactivationTime.length; i++){
+          var tempAct = new Date(task.activationTime[i]);
+          var tempDeact = new Date(task.deactivationTime[i]);
+          timeSpent = tempAct-tempDeact + timeSpent;
+        }
+        if(task.activationTime.length>task.deactivationTime.length){
+          var currentTime = new Date();
+          var tempAct = new Date(task.activationTime[task.activationTime.length-1]);
+          var timeInCurrentSession = currentTime - tempAct;
+          timeSpent = timeSpent + timeInCurrentSession;
+        }
+        $("#timeSpent").text("TIME SPENT: " + Math.round(timeSpent/60000) + " minutes (Extremely experimental)");
+
+        //Add archived Pages
         const archivedPageUrls = task.likedPages;
         const archivedPages = task.history.filter(function(page){
           return (archivedPageUrls.indexOf(page.url)>-1)
