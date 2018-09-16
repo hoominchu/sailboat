2863829acee2abc0c2ce6d81e49d9f961e447a04
hoominchu <hoominchu@gmail.com>
2018-09-16 13:07:33 +0530

    Disabling detect task.

--- contentscripts/dock.js
+++ contentscripts/dock.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
             loadClickLogger();
             loadKeyPressHandler();
             setHighlightIdx();
-            sendDetectTaskMessage();
+            // sendDetectTaskMessage();
         }
     });
 });
