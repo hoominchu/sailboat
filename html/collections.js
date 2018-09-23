0b323c9dc8b87cbe77e2d792632dcdb0a719aac3
hoominchu <hoominchu@gmail.com>
2018-09-23 15:15:15 +0530

    Delete items from collection.

--- html/collections.js
+++ html/collections.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    document.addEventListener("keypress", function(event) {
+$(document).ready(function () {
+    document.addEventListener("keypress", function (event) {
         if (event.keyCode === 13) {
             const newCollectionName = $('#collectionNameInput').val();
             chrome.storage.local.get("Collections", function (collections) {
@@ -7,28 +7,47 @@ $(document).ready(function() {
                 if (!collections.hasOwnProperty(newCollectionName)) {
                     collections[newCollectionName] = {};
                 }
-                chrome.storage.local.set({"Collections":collections});
+                chrome.storage.local.set({"Collections": collections});
                 alert("Collection : " + newCollectionName + " added.");
                 $('#collectionNameInput').val("");
-                chrome.runtime.sendMessage({"type":"collections edited"});
+                chrome.runtime.sendMessage({"type": "collections edited"});
             })
         }
     });
-
     showCurrentCollections();
 });
 
 function showCurrentCollections() {
+    const trashIconURL = chrome.runtime.getURL('images/trash.svg');
     chrome.storage.local.get("Collections", function (collections) {
         collections = collections["Collections"];
+        let $trashIconForCollection = $('<div class="delete_icon"></div>');
+        $trashIconForCollection.css('background-image', 'url(' + trashIconURL + ')');
+        $trashIconForCollection.css('background-size', 'cover');
         const collectionsElem = $('#collections');
         for (let collectionName in collections) {
             let collection = collections[collectionName];
             const $collectionObj = $('<div></div>');
             const $collectionNames = $('<h6 class="text-primary">' + collectionName + '</h6><hr>');
+            $collectionNames.append($trashIconForCollection);
             $collectionObj.append($collectionNames);
             for (let item in collection) {
+                let $trashIconForItem = $('<div class="delete_icon"></div>');
+                $trashIconForItem.css('background-image', 'url(' + trashIconURL + ')');
+                $trashIconForItem.css('background-size', 'cover');
+                $trashIconForItem.click(function () {
+                   const itemName = $(this).parent().text().split('(')[0].trim();
+                   const collectionName = $(this).parent().parent().find('h6').text().trim();
+                   $(this).parent().remove();
+                   chrome.storage.local.get("Collections", function (collections) {
+                       collections = collections["Collections"];
+                       delete collections[collectionName][itemName];
+                       chrome.storage.local.set({"Collections":collections});
+
+                   });
+                });
                 const $item = $('<p>' + item + ' (' + collection[item] + ')' + '</p>');
+                $item.append($trashIconForItem);
                 $collectionObj.append($item);
             }
             collectionsElem.append('<hr>');
