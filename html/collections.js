3d7b409ee43c21bf93ef12cebf872f4c691889af
hoominchu <hoominchu@gmail.com>
2018-09-30 10:55:51 +0530

    Movies collection made much better.

--- html/collections.js
+++ html/collections.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
 
-    // gapi.client.setApiKey('AIzaSyAuPajSr17inQQYo4hXnh6DiWCs84fJXpo');
-    // gapi.client.load('youtube', 'v3', function() {
-    //     console.log('gclient youtube loaded');
-    //     // data = jQuery.parseJSON( '{ "data": [{"name":"orsons"}] }' );
-    //     // $.each(data["data"], function(index, value) {
-    //     //     makeRequest(value["name"]);
-    //     // });
-    // });
+    setTimeout(function () {
+        gapi.client.setApiKey('AIzaSyAuPajSr17inQQYo4hXnh6DiWCs84fJXpo');
+        gapi.client.load('youtube', 'v3', function () {
+            console.log('gclient youtube loaded');
+        });
+    }, 500);
+
 
     document.addEventListener("keypress", function (event) {
         if (event.keyCode === 13) {
@@ -38,7 +37,7 @@ function showCurrentCollections() {
         for (let collectionName in collections) {
             let collection = collections[collectionName];
             const $collectionObj = $('<div></div>');
-            const $collectionNames = $('<h6 class="text-primary">' + collectionName + '</h6><hr>');
+            const $collectionName = $('<h6 class="text-primary">' + collectionName + '</h6><hr>');
             $trashIconForCollection.click(function () {
                 const collectionName = $(this).parent().text().trim();
                 $(this).parent().parent().remove();
@@ -48,11 +47,14 @@ function showCurrentCollections() {
                     chrome.storage.local.set({"Collections": collections});
                 });
             });
-            $collectionNames.append($trashIconForCollection);
-            $collectionObj.append($collectionNames);
+            $collectionName.append($trashIconForCollection);
+            $collectionObj.append($collectionName);
             for (let item in collection) {
-                let $iconGroup = $('<div class="collection-item-icon-group"></div>');
 
+                // Create all the relevant icons group based on the collection name. For example, movies will have imdblink. Books will have goodreads. Places can have maps.
+                let $iconGroup = $('<div class="collection-item-icon-group col-lg-2"></div>');
+
+                // Wikipedia link is for all the collection
                 const wikiLink = 'http://en.wikipedia.org/wiki/' + item.replace(' ', '_');
                 const $wikiIcon = $('<div class="collection-item-icon wiki-icon"></div>');
                 $iconGroup.append($wikiIcon);
@@ -74,13 +76,10 @@ function showCurrentCollections() {
                 }
 
                 if (collectionName.toLowerCase() === 'movies') {
-                    const youtubeLink = 'https://www.youtube.com/embed/' + 'x9f6JaaX7Wg';
                     const $youtubeIcon = $('<div class="collection-item-icon youtube-icon"></div>');
                     $iconGroup.append($youtubeIcon);
                     $youtubeIcon.click(function () {
-                        // search(item);
-                        $('iframe.magicbox-iframe').attr('src', youtubeLink);
-                        $('#magic_modal').modal('show');
+                        searchYoutubeAndSetVideoTarget(item, cleanItemForId(item));
                     });
                 }
 
@@ -104,16 +103,18 @@ function showCurrentCollections() {
 
                     });
                 });
-                let itemCleanedForId = item.replace(/\./g, '');
-                itemCleanedForId = itemCleanedForId.replace(/'/g, '');
-                itemCleanedForId = itemCleanedForId.replace(/,/g, '');
-                itemCleanedForId = itemCleanedForId.replace(/\s/g, '');
-                const $item = $('<p class="collection-item item text-primary" id="' + itemCleanedForId + '">' + item + ' (' + collection[item] + ')' + '</p>');
+                let itemCleanedForId = cleanItemForId(item);
+                const $itemDiv = $('<div class="row collection-div" id="' + itemCleanedForId + '"></div>');
+                $($itemDiv).after('<hr>');
+                const $item = $('<p class="collection-item item text-primary col-lg-9">' + item + ' (' + collection[item] + ')' + '</p>');
 
-                $item.append($trashIconForItem);
-                $item.append($iconGroup);
+                const $itemTitleRow = $('<div class="row col-lg-12 collection-title"></div>');
+                $itemTitleRow.append($item);
+                $itemTitleRow.append($iconGroup);
+                $itemTitleRow.append($trashIconForItem);
+                $itemDiv.prepend($itemTitleRow);
 
-                $collectionObj.append($item);
+                $collectionObj.append($itemDiv);
             }
             collectionsElem.append('<hr>');
             collectionsElem.append($collectionObj);
@@ -122,15 +123,38 @@ function showCurrentCollections() {
     });
 }
 
-function search(q) {
+$('#magic_modal').on('hidden.bs.modal', function () {
+    $('iframe.magicbox-iframe').attr('src', '');
+});
+
+function cleanItemForId(item) {
+    let itemCleanedForId = item.replace(/\./g, '');
+    itemCleanedForId = itemCleanedForId.replace(/'/g, '');
+    itemCleanedForId = itemCleanedForId.replace(/,/g, '');
+    itemCleanedForId = itemCleanedForId.replace(/\s/g, '');
+    return itemCleanedForId;
+}
+
+function searchYoutubeAndSetVideoTarget(q, itemId) {
+    const youtubeLink = 'https://www.youtube.com/embed/';
     const request = gapi.client.youtube.search.list({
-        q: q,
+        q: q + ' trailer',
         part: 'snippet'
     });
 
-    request.execute(function(response) {
-        const str = JSON.stringify(response.result);
-        console.log(str);
+    request.execute(function (response) {
+        if (response) {
+            try {
+                const firstVideoId = response.result['items'][0]['id']['videoId'];
+                const $yticon = $('#' + itemId).find('.youtube-icon');
+                $yticon.attr('video-target', youtubeLink + firstVideoId);
+                $('iframe.magicbox-iframe').attr('src', youtubeLink + firstVideoId);
+                $('#magic_modal').modal('show');
+            } catch (e) {
+                console.log(e);
+            }
+
+        }
     });
 }
 
@@ -158,28 +182,30 @@ function showMovieInfo() {
                     const jsonObj = JSON.parse(jsonText);
                     const $director = $('<div class = "director text-success">' + jsonObj['Director'] + '</div>');
                     $director.click(function () {
-                        const directorWikiLink = 'http://en.wikipedia.org/wiki/' + jsonObj['Director'].replace(/\s/g, '_').replace(/\.',/g,'');
+                        const directorWikiLink = 'http://en.wikipedia.org/wiki/' + jsonObj['Director'].replace(/\s/g, '_').replace(/\.',/g, '');
                         $('iframe.magicbox-iframe').attr('src', directorWikiLink);
                         $('#magic_modal').modal('show');
                     });
                     let movieInfoString = '';
                     if (jsonObj.hasOwnProperty('Genre'))
-                        movieInfoString = jsonObj['Genre'] + ' | ';
+                        movieInfoString = jsonObj['Genre'] + '<br>';
                     if (jsonObj.hasOwnProperty('Language'))
-                        movieInfoString += jsonObj['Language'] + ' | ';
+                        movieInfoString += jsonObj['Language'] + '<br>';
                     if (jsonObj.hasOwnProperty('Runtime'))
-                        movieInfoString += 'Runtime : ' + jsonObj['Runtime'] + ' | <br>';
+                        movieInfoString += 'Runtime : ' + jsonObj['Runtime'] + '<br>';
                     if (jsonObj.hasOwnProperty('Awards'))
-                        movieInfoString += 'Awards : ' + jsonObj['Awards'] + ' | <br>';
+                        movieInfoString += 'Awards : ' + jsonObj['Awards'] + '<br>';
                     if (jsonObj.hasOwnProperty('imdbRating'))
-                        movieInfoString += 'IMDB : ' + jsonObj['imdbRating'] + ' | ';
-                    if (jsonObj.hasOwnProperty('Ratings')){
+                        movieInfoString += 'IMDB : ' + jsonObj['imdbRating'] + '<br>';
+                    if (jsonObj.hasOwnProperty('Ratings')) {
                         if (jsonObj['Ratings'][1])
-                            movieInfoString += 'RT : ' + jsonObj['Ratings'][1]['Value'] + ' | ';
+                            movieInfoString += 'RT : ' + jsonObj['Ratings'][1]['Value'] + '<br>';
                     }
-                    let $movieInfo = $('<div class = "movie-info small">' + movieInfoString + '</div>');
+                    let $movieInfo = $('<div class = "movie-info col-lg-4">' + movieInfoString + '</div>');
                     $movieInfo.prepend($director);
-                    $('#' + movieNameForId).after($movieInfo);
+                    const $moviePlot = $('<div class = "movie-plot col-lg-5">' + jsonObj['Plot'] + '</div>');
+                    const $moviePoster = $('<div class="movie-poster col-lg-3"><img class="movie-poster-img" width="100%" src="' + jsonObj['Poster'] + '"></div>');
+                    $('#' + movieNameForId).append($movieInfo).append($moviePlot).append($moviePoster);
                 }
             };
             xmlhttp.open("GET", omdbRequestURL, true);
